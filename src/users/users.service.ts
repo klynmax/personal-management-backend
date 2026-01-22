483198a9d0408bf77b86de8a33de1320c466ef5a
Klynsman Lemos <klynsman.lemos@itemm.org.br>
2026-01-22 15:16:02 -0300

    Adicionando mais alguns ajustes no prettier

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,1 +1,27 @@
-export class UsersServices {}
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class UsersServices {
+  private users = [
+    {
+      id: 1,
+      name: 'Luiz Inácio',
+      email: 'fazol@email.com',
+      password: '123',
+    },
+    {
+      id: 2,
+      name: 'Flávio Caça Rato',
+      email: 'paidemagrao@email.com',
+      password: '123',
+    },
+    {
+      id: 3,
+      name: 'Eliza Sanches',
+      email: 'sanches_ousadah@email.com',
+      password: '123',
+    },
+  ];
+
+  findAll() {}
+}
