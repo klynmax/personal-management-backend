505bf3fa32cf5ae2d011269b71a25297ccc6cbab
Klynsman Lemos <klynsman.lemos@itemm.org.br>
2026-01-23 11:30:08 -0300

    Configurando a conexão com o BD

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,28 @@
 import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [],
-  controllers: [UsersModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    MongooseModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => {
+        const uri = configService.get<string>('MONGODB_URL');
+
+        if (!uri) {
+          throw new Error('MONGODB_URL is not defined');
+        }
+
+        return { uri };
+      },
+    }),
+    UsersModule,
+  ],
+  controllers: [],
   providers: [],
 })
 export class AppModule {}
