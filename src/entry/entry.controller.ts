e0074e5904409e1b887b631401f5774f155f72f2
Klynsman Lemos <klynsman.lemos@itemm.org.br>
2026-02-04 12:11:22 -0300

    Criando o getAll de entradas

--- src/entry/entry.controller.ts
+++ src/entry/entry.controller.ts
@@ -1,5 +1,20 @@
-import { ApiBadRequestResponse, ApiConsumes, ApiCreatedResponse, ApiOperation, ApiTags } from '@nestjs/swagger';
-import { Body, Controller, Post, Req, UseGuards } from '@nestjs/common';
+import {
+  ApiBadRequestResponse,
+  ApiConsumes,
+  ApiCreatedResponse,
+  ApiOperation,
+  ApiQuery,
+  ApiTags,
+} from '@nestjs/swagger';
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  Query,
+  Req,
+  UseGuards,
+} from '@nestjs/common';
 import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
 import { EntryServices } from './entry.service';
 import { CreateEntryDTO } from './dtos/create-entry.dto';
@@ -12,13 +27,13 @@ export class EntryController {
   constructor(private entryService: EntryServices) {}
 
   @Post()
-    @ApiConsumes('multipart/form-data')
-    @ApiOperation({ summary: 'Cria uma nova entrada.' })
-    @ApiCreatedResponse({ description: 'Entrada criada com sucesso' })
-    @ApiBadRequestResponse({
-      description:
-        'Entrada não pode ser criada. Verifique os campos e tente novamente',
-    })
+  @ApiConsumes('multipart/form-data')
+  @ApiOperation({ summary: 'Cria uma nova entrada.' })
+  @ApiCreatedResponse({ description: 'Entrada criada com sucesso' })
+  @ApiBadRequestResponse({
+    description:
+      'Entrada não pode ser criada. Verifique os campos e tente novamente',
+  })
   create(@Req() req: Request, @Body() body: CreateEntryDTO) {
     const user = req.user as { sub: string };
 
@@ -26,5 +41,23 @@ export class EntryController {
       ...body,
       userId: user.sub,
     });
+  }
+
+  @Get('/all')
+  @ApiQuery({ name: 'page', required: false, example: 1 })
+  @ApiQuery({ name: 'limit', required: false, example: 10 })
+  @ApiOperation({ summary: 'Busca todas as entradas do usuário logado' })
+  findAll(
+    @Req() req: Request,
+    @Query('page') page?: number,
+    @Query('limit') limit?: number,
+  ) {
+    const user = req.user as { sub: string };
+
+    return this.entryService.findAll(
+      user.sub,
+      Number(page) || 1,
+      Number(limit) || 10,
+    );
   }
 }
--- src/entry/entry.service.ts
+++ src/entry/entry.service.ts
@@ -8,15 +8,35 @@ import { CreateEntryDTO } from './dtos/create-entry.dto';
 export class EntryServices {
   constructor(
     @InjectModel(Entry.name)
-    private readonly entryModel: Model<Entry>,
+    private readonly entry: Model<Entry>,
   ) {}
 
   create(data: CreateEntryDTO & { userId: string }) {
-    const newEntry = new this.entryModel({
+    const newEntry = new this.entry({
       ...data,
       userId: data.userId,
     });
 
     return newEntry.save();
+  }
+
+  async findAll(userId: string, page = 1, limit = 10) {
+    const skip = (page - 1) * limit;
+
+    const filter = { deleted: false, userId };
+    const [data, total] = await Promise.all([
+      this.entry.find(filter).skip(skip).limit(limit).lean().exec(),
+      this.entry.countDocuments(filter),
+    ]);
+
+    return {
+      data,
+      meta: {
+        total,
+        page,
+        limit,
+        totalPage: Math.ceil(total / limit),
+      },
+    };
   }
 }
